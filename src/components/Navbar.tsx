49466b7146256a622a3af02ad2e95061a3ba521a
Henrique.Cost <henrique.costa@ekaizen.digital>
2022-08-03 15:28:40 -0300

    fix: home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,12 +8,12 @@ export default function Navbar() {
     <title>Navbar</title>
   </head>
   <header>
-      <nav className="navbar navbar-expand-lg bg-light">
+      <nav className="navbar navbar-expand-lg bg-slate-300">
       <div className="container-fluid">
         <a className="navbar-brand" href='/'>Home</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse flex justify-end" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link" aria-current="page" href='/personal-projects'>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Code } from 'phosphor-react';
+import { Code, Gear } from 'phosphor-react';
 import React from 'react';
 import '/home/henrique/Documents/my-portifolio/src/pages/Home/style.css';
 
@@ -18,37 +18,37 @@ return(
         <div className="mt-[130px]">
             <p className="text-7xl font-sans ">Welcome to my portfolio</p><br/>
             <p className="text-lg  font-sans ml-32">it's not lack of coffee or tequila, it's the code that doesn't compile...</p>
-            <button className="bg-green-400 p-3 opacity-60  mt-[120px] ml-[260px] font-sans text-lg hover:opacity-100 hover:text-white">Learn more and start today</button>
+            <button className="bg-blue-400 p-3 opacity-60  mt-[120px] ml-[260px] font-sans text-lg hover:opacity-100 hover:text-white rounded-full">Learn more and start today</button>
         </div>
     </div> 
 </section> 
-    <section className='bg-green-500 p-24'>
+    <section className='bg-blue-500 p-24 h-[600px]'>
         <span id='comece' className='text-5xl font-sans text-white flex justify-center'>Hello, i'm Henrique</span><br />
         <p className=" px-[300px] text-xl text-white font-sans text-center">I started programming at 16 years old, my first impression was already "I want this for life", I started in visualG30, notepad++, but I always liked the backend,
             later I got to know my favorite language JAVA, with JAVA I felt comfortable to program, I identified with the language.
         </p>
     </section>
 
-<section className='flex mt-[-14rem] bg-black text-center pb-1 p-0'>
+<section className='flex text-center h-[900px]'>
     <div className='container max-w-[1344]'>
-        <div className='rounded-xl flex bg-slate-600'>
+        <div className='rounded-xl bg-white h-[850px] grid grid-rows-1 grid-cols-3 mt-[-200px] border-4'>
             <div className="">
-            <span className=''><Code size={85} /></span>
+            <span className='flex justify-center'><Gear size={85} /></span>
             <h1>Row column</h1>
             </div>
-            <div className="ml-6">
-            <span><Code size={85} /></span>
+            <div className="border-x-4 border-x-slate-300">
+            <span className='flex justify-center'><Code size={85} /></span>
             <h1>Row column</h1>
             </div>
-            <div className="ml-6">
-            <span><Code size={85} /></span>
-            <h1>Row column</h1>
+            <div className="">
+            <span className='flex justify-center'><Code size={85} /></span>
+            <h1>Row column </h1>
             </div>
         </div>
     </div>
     </section>  
 </body>
-<footer>
+<footer className='h-[200px] text-center'>
     cabeçalho
 </footer>
 </html>  
